297b6f2b5d81fbfccafe07c3584eed09adbc8386
umarbit <umarbit87@gmail.com>
2017-07-28 00:35:12 -0400

    Second commit with the new activity

--- app/src/main/java/com/example/umar/testapplication/MyBroadcastManager.java
+++ app/src/main/java/com/example/umar/testapplication/MyBroadcastManager.java
@@ -11,6 +11,6 @@ import android.content.Intent;
 public class MyBroadcastManager extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        
+
     }
 }
