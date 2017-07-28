4789f36725a5c8d8fe8713613fa11de4d98e0d99
umarbit <umarbit87@gmail.com>
2017-07-28 00:32:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/umar/testapplication/MyBroadcastManager.java
@@ -0,0 +1,16 @@
+package com.example.umar.testapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Umar on 7/28/2017.
+ */
+
+public class MyBroadcastManager extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
